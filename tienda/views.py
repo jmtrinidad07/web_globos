19da8d98c58239636daf703dc715a63ff8ca10c8
Juan Trinidad <jmtrinida07@gmail.com>
2023-08-12 13:08:44 -0600

    tienda added

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Mi negocio web<h1>")
+def index_tienda(request):
+    return HttpResponse("<h1>Bienvenido a la tienda<h1>")
