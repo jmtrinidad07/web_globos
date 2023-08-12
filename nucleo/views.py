22bfd94a807b00110fe195cf65b4460c7fb30952
Juan Trinidad <jmtrinida07@gmail.com>
2023-08-12 13:56:43 -0600

    Static files added

--- nucleo/views.py
+++ nucleo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Mi negocio web<h1>")
+    return render(request,"nucleo/index.html")
